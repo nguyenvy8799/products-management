c83ab3ad2efab7cafae24006b6b4e1121068e766
Tu Nguyen Huu <nhtu@Tus-MacBook-Pro-2.local>
2024-05-15 19:30:16 +0700

    Lesson 11 - 1

--- src/app.js
+++ src/app.js
@@ -26,21 +26,83 @@ app.set("views", "views/pages");
 
 // Cau hinh static files
 app.use(express.static("public"));
+// Cau hinh parse Req body
+app.use(express.urlencoded());
 
 connectMongoDB();
 
 // Route render trang products
 app.get("/products", async (req, res) => {
-  const products = await ProductModel.find();
-  const categories = await CategoryModel.find();
-
-  console.log("Products", products);
+  const products = await ProductModel.find().lean();
+  const categories = await CategoryModel.find().lean();
 
   res.render("products", {
     pageCode: "product",
-    products: JSON.parse(JSON.stringify(products)),
-    categories: JSON.parse(JSON.stringify(categories)),
+    products,
+    categories,
   });
+});
+
+// Route render trang Tao danh muc
+app.get("/products/create-category", (req, res) => {
+  res.render("create-category", {
+    pageCode: "product",
+  });
+});
+
+app.post("/products/create-category", async (req, res) => {
+  const data = req.body;
+  console.log("Data: ", data);
+
+  // Cach 1: Tao Category document & dung method Document.save()
+  // const category = new CategoryModel({
+  //   name: data.name,
+  //   desc: data.desc,
+  // });
+  // await category.save();
+
+  // Cach 2: Dung method CategoryModel.create()
+  await CategoryModel.create({
+    name: data.name,
+    desc: data.desc,
+  });
+
+  res.redirect("/products");
+});
+
+app.get("/products/category/:id", async (req, res) => {
+  const id = req.params.id;
+  const category = await CategoryModel.findById(id).lean();
+  console.log("Category: ", category);
+
+  res.render("create-category", {
+    pageCode: "product",
+    category,
+    isEditing: true,
+  });
+});
+
+app.post("/products/category/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  console.log("Data: ", data);
+
+  // Cap nhat du lieu moi cho danh muc trong MongoDB
+  await CategoryModel.updateOne(
+    // Filter object
+    {
+      _id: id,
+    },
+    // Update object
+    {
+      $set: {
+        name: data.name,
+        desc: data.desc,
+      },
+    }
+  );
+
+  res.redirect("/products");
 });
 
 // Run app
